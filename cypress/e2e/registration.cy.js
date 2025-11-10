457ece3e85ecb7a40e8cff3dc4378ce5cf027ad8
Mykhailo <kurochkinmy@gmail.com>
2025-11-10 10:08:44 +0100

    test: add tests for form fields

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,11 +1,65 @@
 /// <reference types='cypress' />
 
 describe('Student Registration page', () => {
-  before(() => {
+  const form = {
+    firstName: 'Bob',
+    lastName: 'Smith',
+    email: 'bob.smith@example.com',
+    gender: 'Male',
+    mobile: '1234567890',
+    dateOfBirth: '10 November,2008',
+    subjects: ['Math', 'English'],
+    hobbies: ['Sports', 'Reading', 'Music'],
+    address: '123 Main St, Springfield',
+    state: 'Uttar Pradesh',
+    city: 'Merrut'
+  };
 
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/automation-practice-form');
   });
 
-  it('', () => {
+  it('should require main fields', () => {
+    cy.get('input#firstName').should('have.attr', 'required');
+    cy.get('input#lastName').should('have.attr', 'required');
+    cy.get('input#userNumber').should('have.attr', 'required');
+    cy.get('input#gender-radio-1').should('have.attr', 'required');
+    cy.get('input#gender-radio-2').should('have.attr', 'required');
+    cy.get('input#gender-radio-3').should('have.attr', 'required');
+  });
 
+  it('should save info from form to modal', () => {
+    cy.get('input#firstName').type(form.firstName);
+    cy.get('input#lastName').type(form.lastName);
+    cy.get('input#userEmail').type(form.email);
+    cy.contains('label', form.gender).click();
+    cy.get('input#userNumber').type(form.mobile);
+    cy.get('input#dateOfBirthInput').click();
+    // use text input once error
+    cy.get('.react-datepicker__year-select').select('2008');
+    cy.get('.react-datepicker__month-select').select('November');
+    cy.get('[aria-label="Choose Monday, November 10th, 2008"]').click();
+    form.subjects.forEach((subject) => {
+      cy.get('input#subjectsInput').type(subject + '{enter}');
+    });
+    cy.contains('Sports').click();
+    cy.contains('Reading').click();
+    cy.contains('Music').click();
+
+    cy.get('textarea#currentAddress').type(form.address);
+    cy.contains('Select State').click();
+    cy.contains('div', 'NCR').click();
+    cy.contains('Select City').click();
+    cy.contains('div', 'Delhi').click();
+    cy.get('button#submit').click();
+
+    cy.get('.modal-body').should('contain', form.firstName);
+    cy.get('.modal-body').should('contain', form.lastName);
+    cy.get('.modal-body').should('contain', form.email);
+    cy.get('.modal-body').should('contain', form.gender);
+    cy.get('.modal-body').should('contain', form.mobile);
+    cy.get('.modal-body').should('contain', form.dateOfBirth);
+    cy.get('.modal-body').should('contain', form.hobbies.join(', '));
+    cy.get('.modal-body').should('contain', form.address);
   });
 });
